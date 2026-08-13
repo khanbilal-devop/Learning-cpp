edc0ccb943e72eaab61c7179976d0f19e1c43665
Ahmed Khan <khanbial13@gmail.com>
2026-08-13 18:14:04 -0400

    Update vectors_with_complex.cpp

--- vectors_with_complex.cpp
+++ vectors_with_complex.cpp
@@ -20,11 +20,11 @@ int main() {
     points.emplace_back(7.2, 8.9);
 
     std::vector<std::complex<double>>::iterator ptr = points.begin();
-    std::cout << "Start element of the buffer : " << *ptr << std::endl;
-    std::cout << "Second element imaginary of the buffer : " << (*(ptr+1)).imag() << std::endl;
+    std::cout << "Start element of the buffer : " << ptr->imag() << std::endl;
+    std::cout << "Second element imaginary of the buffer : " << (ptr+1)->imag() << std::endl;
     std::cout << "Third element of the buffer : " << *(ptr+2) << std::endl;
     std::cout << "Fourth element of the buffer : " << *(ptr+3) << std::endl;
-    
+
     std::cout << "The first real part: " << points.begin()->real() << std::endl;
     std::cout << "Imaginary part at index 1: " << points[1].imag() << std::endl;
     std::cout << "Next to last real: " << prev(points.end(), 2)->real() << std::endl;
